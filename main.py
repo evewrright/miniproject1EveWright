b4f042009f99b71fffc5e594e81514c20ba4e962
Eve Wright <e_wright5@mail.fhsu.edu>
2024-02-11 11:02:54 -0600

    Add loop for tickers to retrieve close price in given date interval

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ import yfinance as yf
 #                the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt
-#                file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
+#                file which contains all the packages that need installed. You can create this fille with the output
+#                of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the
 #                pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+
+tickers = ['EL']
+
+for ticker in tickers:
+    myticker = yf.Ticker(ticker)
+    myhistory = myticker.history(start='2024-01-31', end='2024-02-09')
+    myclose = myhistory['Close'][ticker]
+    
 
